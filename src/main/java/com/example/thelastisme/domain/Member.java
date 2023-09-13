106b4f4776314543ae1d9940aebf7cd5f9c7a6c4
Yuje Lee <dbwp031@gmail.com>
2023-09-13 19:34:38 +0900

    #18 - oauth: Member 도메인 추가
    MemberDto는 사용하지 않음!!!!!
    user_account는 이미 늦음. -> 그냥 정보 담는 도메인으로 변경
    
    사용자의 진짜 정보를 담기 위한 도메인 추가

--- src/main/java/com/example/thelastisme/domain/Member.java
+++ src/main/java/com/example/thelastisme/domain/Member.java
@@ -1,13 +1,19 @@
 package com.example.thelastisme.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.thelastisme.auth.enums.AuthProviderType;
+import com.example.thelastisme.auth.enums.MemberRole;
+import com.example.thelastisme.repository.RoleRepository;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @Getter
+@Setter
+@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
@@ -16,6 +22,26 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    private String nickname;
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<Role> roles = new ArrayList<>();
+
+    private String name;
     private String email;
+
+    public void setRoles(List<MemberRole> roleEnums, RoleRepository roleRepository) {
+        List<Role> newRoles = roleEnums.stream().map((roleEnum) -> Role.builder()
+                .memberRole(roleEnum).member(this).build()).collect(Collectors.toList());
+        this.roles = newRoles;
+    }
+    public void setRoles(List<MemberRole> roleEnums) {
+        List<Role> newRoles = roleEnums.stream().map((roleEnum) -> Role.builder()
+                .memberRole(roleEnum).member(this).build()).collect(Collectors.toList());
+        this.roles = newRoles;
+    }
+
+    public Member update(String name) {
+        this.name = name;
+        return this;
+    }
 }
